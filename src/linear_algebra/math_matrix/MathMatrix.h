666f708cc831d37fd03a43f64672d9a643c82975
Connor W <connorw011@gmail.com>
2016-05-02 14:48:27 -0500

    Chaned the MathMatrix header file for trimmed down interface

--- src/linear_algebra/math_matrix/MathMatrix.h
+++ src/linear_algebra/math_matrix/MathMatrix.h
@@ -20,10 +20,10 @@ template <class T>
 class MathMatrix : public BaseMathMatrix<T, MathMatrix>
 {
   public:
-
     MathMatrix() : myColumns(0) {}
-    MathMatrix(size_t rows, size_t cols);
+    explicit MathMatrix(size_t rows, size_t cols = 1);
     MathMatrix(const MathMatrix& other);
+    MathMatrix(const IMathMatrix<T>& other);
     MathMatrix(MathMatrix&& other);
     ~MathMatrix();
 
@@ -31,37 +31,44 @@ class MathMatrix : public BaseMathMatrix<T, MathMatrix>
     MathMatrix<T>& operator=(MathMatrix rhs);
 
     bool opEquality(const IMathMatrix<T>& rhs) const;
+    bool operator==(const MathMatrix& rhs) const;
+    bool operator!=(const MathMatrix& rhs) const;
 
-    MathMatrix<T>& opPlusEquals(const IMathMatrix<T>& rhs);
-    MathMatrix<T>& opMinusEquals(const IMathMatrix<T>& rhs);
+    MathMatrix& operator+=(const IMathMatrix<T>& rhs);
+    MathMatrix& operator-=(const IMathMatrix<T>& rhs);
+    MathMatrix& operator*=(const IMathMatrix<T>& rhs);
+    MathMatrix& operator*=(const T& scaler);
 
-    MathMatrix<T>& opTimesEquals(const T& scaler);
+    MathMatrix operator+(const IMathMatrix<T>& rhs) const;
+    MathMatrix operator-(const IMathMatrix<T>& rhs) const;
+    MathMatrix operator-() const;
+    MathMatrix operator*(const IMathMatrix<T>& rhs) const;
+    MathMatrix operator*(const T& scaler) const;
+    MathVector<T> operator*(const MathVector<T>& rhs) const;
 
-    MathMatrix<T>* opPlus(const IMathMatrix<T>& rhs) const;
-    MathMatrix<T>* opMinus(const IMathMatrix<T>& rhs) const;
-    MathMatrix<T>* opMinus() const;
-    MathMatrix<T>* opTimes(const IMathMatrix<T>& rhs) const;
-    MathMatrix<T>* opTimes(const T& scaler) const;
+    MathMatrix transpose() const;
+    MathMatrix swapRows(size_t row1, size_t row2);
 
-    MathVector<T>& at(size_t index);
-    const MathVector<T>& at(size_t index) const;
     T& at(size_t row, size_t column);
     const T& at(size_t row, size_t column) const;
 
     size_t getRows() const;
     size_t getCols() const;
 
-    void swapRows(size_t row1, size_t row2);
+    void swap(MathMatrix& other);
     void printToStream(std::ostream& os) const;
     void readFromStream(std::istream& is);
 
   private:
-    void swap(MathMatrix& other);
-
     Array<MathVector<T>*> myRows;
-    int myColumns;
-    constexpr static int DEFAULT_SIZE = 2;
+    size_t myColumns;
 };
+
+template <class T>
+MathMatrix<T> operator*(const T& scaler, MathMatrix<T> matrix)
+{
+  return matrix *= scaler;
+}
 
 #include "MathMatrix.hpp"
 
